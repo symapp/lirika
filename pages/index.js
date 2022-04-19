ca1c0f22fcde189288abd22b8e718c228a5fbb0a
bdiass <bdiass@bbcag.ch>
2022-04-19 10:26:38 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "./index.module.css"
 export default function IndexPage() {
     return (
         <div className={styles.posts}>
-            <h1>Welcome to my project!</h1>
+            <h1>Welcome to my project!asdf</h1>
         </div>
     )
 }
